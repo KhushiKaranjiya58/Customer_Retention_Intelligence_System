f6ace996157beb060f69417bd1e05c5e75ef8329
Khushi Karanjiya <khushikaranjiya2@gmail.com>
2026-06-21 17:29:32 +0530

    Fixed model loading path

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,12 @@
+from pathlib import Path
 import streamlit as st
 import joblib
 
 @st.cache_resource
 def load_model():
-    return joblib.load("models/churn_model.pkl")
+
+    project_root = Path(__file__).resolve().parent.parent
+
+    model_path = project_root / "models" / "churn_model.pkl"
+
+    return joblib.load(model_path)
